ba7e76994d8b1bebb07203a5db5f8fce2d5574d4
Kishan <kishan.nr@hashedin.com>
2020-04-07 16:05:21 +0530

    updated urls for views

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
 
 
+@api_view(['GET'])
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
